491a0b8e232b04e1ef67c46ae047f095b85e9928
root <root@debian>
2013-07-05 14:04:20 +0200

    cece

--- modules/menu/javascripts/menu.js
+++ modules/menu/javascripts/menu.js
@@ -5,11 +5,11 @@ $(function(){
 
 //
 function highlightIconMenu(ref){
-	//get all menu div and set color
+	//
 	$(".menu_div").each(function(i,e){
 		e.style.backgroundColor="#dddddd";
 	});
-	//set color on selected
+	//
 	ref.style.backgroundColor="yellow";
 }
 
