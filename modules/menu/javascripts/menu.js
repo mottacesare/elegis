7b5607d0d73478ffa2158e35fb43578f63ce1fa6
root <root@debian>
2013-07-05 11:34:49 +0200

    cece

---
+++ modules/menu/javascripts/menu.js
@@ -0,0 +1,39 @@
+	//highligt
+	/*var menu_div=document.getElementsByClassName("menu_div");
+	//
+	.each(function(e,i){
+	console.log(menu_div);*/
+	
+	/*//highlight selected
+	elements=[];
+	$$("tr:.row").each(function(e,i){
+		if(e.get("href")){
+			e.addEvent("click",function(ev){
+				resetAll();
+				//
+				e.set("class","darkgrey");
+				//
+				parent.main.location.href=e.get("href");
+			});
+			elements.push(e);
+		}
+	});
+	//accordion
+	
+	var ac = new Fx.Accordion(
+		$$('.togglers'), 
+		$$(".elements"), 
+		{
+			show: -1,
+			initialDisplayFx: false,
+			width:false,
+			opacity:false,
+		}
+		);*/
+
+//
+function highlightIconMenu(ref){
+	//get all menu div
+	console.log($(".menu_div"));
+}
+
