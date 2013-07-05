546828de88b7d66d114c46c9595c60bba672cbff
root <root@debian>
2013-07-05 12:44:29 +0200

    cece

--- modules/menu/javascripts/menu.js
+++ modules/menu/javascripts/menu.js
@@ -1,39 +1,15 @@
-	//highligt
-	/*var menu_div=document.getElementsByClassName("menu_div");
-	//
-	.each(function(e,i){
-	console.log(menu_div);*/
+//on load
+$(function(){
 	
-	/*//highlight selected
-	elements=[];
-	$$("tr:.row").each(function(e,i){
-		if(e.get("href")){
-			e.addEvent("click",function(ev){
-				resetAll();
-				//
-				e.set("class","darkgrey");
-				//
-				parent.main.location.href=e.get("href");
-			});
-			elements.push(e);
-		}
-	});
-	//accordion
-	
-	var ac = new Fx.Accordion(
-		$$('.togglers'), 
-		$$(".elements"), 
-		{
-			show: -1,
-			initialDisplayFx: false,
-			width:false,
-			opacity:false,
-		}
-		);*/
+});
 
 //
 function highlightIconMenu(ref){
-	//get all menu div
-	console.log($(".menu_div"));
+	//get all menu div and set color
+	$(".menu_div").each(function(i,e){
+		e.style.backgroundColor="#dddddd";
+	});
+	//set color on selected
+	ref.style.backgroundColor="yellow";
 }
 
