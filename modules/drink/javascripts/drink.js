d60ccbc941f6f9ad02aa532e801ddb9c40003ac7
root <root@debian>
2013-07-08 14:21:29 +0200

    cece

--- modules/drink/javascripts/drink.js
+++ modules/drink/javascripts/drink.js
@@ -3,18 +3,27 @@ $(function(){
 	
 });
 
-//list checkbox
+//checkbox selected
 function selCheckbox(ref){
-	//
+	//get all checkbox
 	var sel=$(ref).find('input')[0];
-	//
+	var check_color="";
+	//check if selected or not
 	if(sel.value==0){
 		sel.value=1;
-		$(ref).css('background-color','#aaaaaa');
+		check_color='#aaaaaa';
+		td_color='#aaaaaa';
 	}else{
 		sel.value=0;
-		$(ref).css('background-color','#f7f7f7');
+		check_color='#f7f7f7';
+		td_color='#f7f7f7';
 	}
+	//change color on checkbox and on row
+	$(ref).css('background-color',check_color);
+	//
+	$(ref).siblings().each(function(i,e){
+		$(e).css('background-color',td_color);
+	});
 }
 
 //img menu
