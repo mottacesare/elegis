4d57d95cba86efb92472eae87b6be24c5bc487c4
root <root@debian>
2013-07-13 11:51:24 +0200

    cece

--- modules/table/list_table.php
+++ modules/table/list_table.php
@@ -3,11 +3,11 @@
 require_once("../../include.php");
 //
 $l->createHeader();
-//$l->addJavascripts(array("javascripts/list_table.js"));
+$l->addJavascripts(array("javascripts/list_table.js"));
 $l->createPageTitleBar($def_title,$dict["TITLE_TABLE"]);
 //
 //######################################################################################
-echo "<embed type=\"image/svg+xml\" src=\"../../resources/plan.svg\" />";
+echo "<embed id=\"svg_plan\" type=\"image/svg+xml\" src=\"../../resources/plan.svg\"/>";
 //
 //
 $l->createFooter();
