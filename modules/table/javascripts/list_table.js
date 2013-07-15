57655f155fe489b6c8191ed13b9d91fb3401cd4c
root <root@debian>
2013-07-15 12:09:37 +0200

    cece

--- modules/table/javascripts/list_table.js
+++ modules/table/javascripts/list_table.js
@@ -1,4 +1,7 @@
-//on load
-$(function(){
-	plan=$("#svg_plan");
+$(window).load(function(){
+	var svg=document.getElementById("svg_plan").getSVGDocument();
+	//
+	
+	t01=svg.getElementById("T01");
+	t01.style.setProperty("fill","red", "");
 });
