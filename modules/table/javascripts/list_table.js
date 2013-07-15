f2722be6c820c26e9eb39b4d6764568b9b4e09e8
root <root@debian>
2013-07-15 14:50:43 +0200

    cece

--- modules/table/javascripts/list_table.js
+++ modules/table/javascripts/list_table.js
@@ -1,7 +1,22 @@
 $(window).load(function(){
-	var svg=document.getElementById("svg_plan").getSVGDocument();
+	var svg=$(document.getElementById("svg_plan").getSVGDocument());
 	//
+	svg.find(".svg_table").each(function(i,e){
+		//
+		$(e).mouseover(function(ref){
+			console.log(ref);
+		});
+		//
+		$(e).mouseout(function(ref){
+			console.log(ref);
+		});
+		//
+		$(e).click(function(ref){
+			console.log(ref);
+		});
+	});
 	
-	t01=svg.getElementById("T01");
-	t01.style.setProperty("fill","red", "");
+	
+	/*t01=svg.getElementById("T01");
+	t01.style.setProperty("fill","red", "");*/
 });
